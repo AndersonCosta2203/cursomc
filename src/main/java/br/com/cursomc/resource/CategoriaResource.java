8c05106606e2fab53c3368dd143f3a342d62991b
Anderson Costa <anderson.costa2203@gmail.com>
2018-04-19 23:58:55 -0300

    Testando a primeira classe categoria

--- src/main/java/br/com/cursomc/resource/CategoriaResource.java
+++ src/main/java/br/com/cursomc/resource/CategoriaResource.java
@@ -1,8 +1,12 @@
 package br.com.cursomc.resource;
 
+import br.com.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController //Significa que a classe será um controlador Rest
@@ -10,7 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REST está funcionando";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.add(cat1);
+        categorias.add(cat2);
+
+        return categorias;
     }
 }
