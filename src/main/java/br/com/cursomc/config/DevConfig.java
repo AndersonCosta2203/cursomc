ac716cd5c6b7ef167c151169348d6bec0b70d045
Anderson Costa <anderson.costa2203@gmail.com>
2018-06-10 10:38:36 -0300

    Criando Profile de Dev

---
+++ src/main/java/br/com/cursomc/config/DevConfig.java
@@ -0,0 +1,32 @@
+package br.com.cursomc.config;
+
+import br.com.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev") // Para utilizar o profile de test configurado no arquivo application.properties
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean // Indica que o Spring deve invocar este método e gerenciar o objeto retornado por ele.
+    public boolean instantiateDatabase() throws ParseException {
+
+        if (!"create".equals(strategy)) {
+            return false;
+        }
+
+        dbService.instantiateTestDataBase();
+        return true;
+    }
+}
