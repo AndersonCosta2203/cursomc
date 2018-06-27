451e858b43bca2e73ba1ebdb9d1ce98bcb0875e1
Anderson Costa <anderson.costa2203@gmail.com>
2018-06-27 07:07:46 -0300

    adicionando img diagrama

---
+++ src/main/java/br/com/cursomc/config/S3Config.java
@@ -0,0 +1,11 @@
+package br.com.cursomc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Bean
+    public AmazonS3
+}
